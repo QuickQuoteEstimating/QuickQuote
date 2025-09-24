8689be3213aecbc31791fca281f53ae5ab08b9c5
QuickQuoteEstimating <info.quickquote@gmail.com>
2025-09-24 16:52:48 +0000

    Remove old app folder, keep new quickquote structure

--- quickquote/app/(tabs)/customers.tsx
+++
@@ -1,8 +0,0 @@
-import { View, Text } from 'react-native';
-export default function Customers() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Customers</Text>
-    </View>
-  );
-}
