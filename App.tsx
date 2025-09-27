56171a9890bd3ee8df55ee61b2ceda3825b0b9e6
QuickQuoteEstimating <info.quickquote@gmail.com>
2025-09-27 19:32:56 -0400

    Add ReadableStream polyfill for Expo start

--- App.tsx
+++ App.tsx
@@ -1,1 +1,7 @@
+import { ReadableStream as PolyfillReadableStream } from "web-streams-polyfill/ponyfill";
+
+if (typeof globalThis.ReadableStream === "undefined") {
+  globalThis.ReadableStream = PolyfillReadableStream;
+}
+
 import "expo-router/entry";
