170cbd24fb82803fe2468f9ec616e22c75f14b3e
QuickQuoteEstimating <info.quickquote@gmail.com>
2025-10-02 14:52:59 +0000

    fixed customers error

--- app/(tabs)/customers.tsx
+++ app/(tabs)/customers.tsx
@@ -428,7 +428,7 @@ export default function Customers() {
                 previousCustomersRef.current = null;
               }
             })();
-          },
+          
         },
       );
     },
